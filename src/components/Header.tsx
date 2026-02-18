6cf1469a663073305551d221df30bc9aa095a5ae
tsoumdoa <8blondes.swerves@icloud.com>
2026-02-18 13:19:02 +0800

    link to github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
 interface HeaderProps {
-  onSettingsClick: () => void
+	onSettingsClick: () => void
 }
 
 export default function Header({ onSettingsClick }: HeaderProps) {
-  return (
-    <div className="flex items-center justify-between">
-      <h1 className="text-start text-2xl font-bold">
-        C5 Hopper - Grasshopper C# Generator
-      </h1>
-      <button
-        onClick={onSettingsClick}
-        className="ml-4 rounded border-2 border-black bg-white px-3 py-2 text-sm font-bold hover:bg-gray-100 focus:ring-1 focus:ring-black/50 focus:ring-offset-1 focus:outline-none"
-      >
-        Settings
-      </button>
-    </div>
-  )
+	return (
+		<div className="flex items-center justify-between">
+			<h1 className="text-start text-2xl font-bold">
+				<a href="https://github.com/tsoumdoa/c5hopper" target="_blank" rel="noreferrer">
+					C5 Hopper - Grasshopper C# Generator
+				</a>
+			</h1>
+			<button
+				onClick={onSettingsClick}
+				className="ml-4 rounded border-2 border-black bg-white px-3 py-2 text-sm font-bold hover:bg-gray-100 focus:ring-1 focus:ring-black/50 focus:ring-offset-1 focus:outline-none"
+			>
+				Settings
+			</button>
+		</div>
+	)
 }
